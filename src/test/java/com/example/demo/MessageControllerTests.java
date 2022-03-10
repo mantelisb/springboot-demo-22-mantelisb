7473ba7b29a721061fe6adc70709c3fa81002c57
Mantas Boronilščikovas <mantas.boronilscikovas@swedbank.lt>
2022-03-10 15:42:42 +0200

    fix: failing controller test

--- src/test/java/com/example/demo/MessageControllerTests.java
+++ src/test/java/com/example/demo/MessageControllerTests.java
@@ -30,7 +30,7 @@ public class MessageControllerTests {
     @Test
     public void shouldBEandDBWork() {
         when(repository.findAll()).thenReturn(Arrays.asList(new Message(MESSAGE)));
-        assertEquals("MessageController.BE_SUCCESS_MESSAGE" + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.getMessage());
+        assertEquals(MessageController.BE_SUCCESS_MESSAGE + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.getMessage());
     }
     @Test
     public void shouldWorkOnlyBE() {
